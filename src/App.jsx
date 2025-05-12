2378753aa9690166cafd4d5726c65ce7f255e732
Anvarbek <c1221sharp@gmail.com>
2025-05-12 18:05:32 +0500

    feat: Contact is ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import styled from "styled-components";
 import "./App.css";
 import MainPage from "./components/MainPage";
+import Contact from "./components/Contact";
 
 const Wrapper = styled.div`
   width: 100%;
+  font-family: Georgia, 'Times New Roman', Times, serif;
 `;
 
 function App() {
   return (
     <Wrapper>
       <MainPage />
+      <Contact />
     </Wrapper>
   );
 }
