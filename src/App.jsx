eb591e1a7ef849026c40cb20d86efc4fa0cf3f03
Anvarbek <c1221sharp@gmail.com>
2025-05-12 14:46:46 +0500

    feat: MainPage is ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+import styled from "styled-components";
 import "./App.css";
 import MainPage from "./components/MainPage";
 
+const Wrapper = styled.div`
+  width: 100%;
+`;
+
 function App() {
   return (
-    <>
+    <Wrapper>
       <MainPage />
-    </>
+    </Wrapper>
   );
 }
 
--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,12 +1,33 @@
+import styled from "styled-components";
 
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-color: #1e1e1e;
+  background-image: url("https://i.postimg.cc/BnbhpttX/pic1.jpg");
+  background-size: cover;
+  background-position: center center;
+  background-repeat: no-repeat;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+`;
 
+const H1 = styled.h1`
+  font-size: 4.5rem;
+  color: gold;
+  text-shadow: 2px 2px 4px #000;
+  padding: 10px 10px;
+  border-radius: 8px;
+`;
 
 const MainPage = () => {
-    return (
-        <>
-        <h1>Hello</h1>
-        </>
-    );
-}
+  return (
+    <Wrapper>
+      <H1>Alisher's Barbershop</H1>
+    </Wrapper>
+  );
+};
 
 export default MainPage;
